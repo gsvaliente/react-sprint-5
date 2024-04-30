0d3d2828570b602311a09057a4c7f7bf492e2bd2
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-04-30 20:15:46 +0200

    Created the card using DaisyUI and custom components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,65 @@
-import { TutorialData } from "../types/tutorialData.type";
+import { tutorialData } from '../data/tutorialData';
+import { TutorialData } from '../types/tutorialData.type';
+import { ArrowButton } from './ArrowButton';
+import { Indicator } from './Indicator';
 
 type CardProps = {
+  currStep: number;
   data: TutorialData;
   onNextStep: () => void;
+  onPrevStep: () => void;
 };
 
-export const Card = ({ data, onNextStep }: CardProps) => {
+export const Card = ({ data, onNextStep, onPrevStep, currStep }: CardProps) => {
   const { bgColor, description, image, title } = data;
-  return (
-    <div>
-      <h1>{title}</h1>
-      <img src={image} alt={title} />
-      <p>{description}</p>
+  console.log(currStep);
 
-      <button onClick={onNextStep}>Next</button>
+  return (
+    <div className='card w-96 max-w-80 md:max-w-96 h-xl bg-base-100 shadow-xl bordered-t-'>
+      <div
+        style={{ background: bgColor }}
+        className='rounded-t-2xl'
+      >
+        <figure className='min-h-82 md:min-h-96'>
+          <img
+            src={image}
+            alt={title}
+          />
+        </figure>
+      </div>
+      <div className='card-body'>
+        <h2 className='card-title'>{title}</h2>
+        <p>{description}</p>
+        <div className='flex absolute bottom-10'>
+          {tutorialData.map((el, idx) => {
+            return (
+              <Indicator
+                key={el.title}
+                currStep={currStep}
+                index={idx}
+              />
+            );
+          })}
+        </div>
+        <div className='card-actions justify-end'>
+          {currStep === 2 && (
+            <ArrowButton
+              func={onPrevStep}
+              position='left'
+            />
+          )}
+          {currStep === 1 && (
+            <>
+              <ArrowButton
+                func={onPrevStep}
+                position='left'
+              />
+              <ArrowButton func={onNextStep} />
+            </>
+          )}
+          {currStep === 0 && <ArrowButton func={onNextStep} />}
+        </div>
+      </div>
     </div>
   );
 };
