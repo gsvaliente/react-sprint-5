ae4fb33d422b97a653e0b70a38d0f8f3dc02eeae
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-04-30 20:15:59 +0200

    Fixed minor issues with the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { Card } from "./components/Card";
-import { tutorialData } from "./data/tutorialData";
+import { useState } from 'react';
+import { Card } from './components/Card';
+import { tutorialData } from './data/tutorialData';
 
 function App() {
   const [step, setStep] = useState<number>(0);
@@ -9,11 +9,19 @@ function App() {
     setStep((curr) => curr + 1);
   }
 
+  function handlePreviousStep() {
+    setStep((curr) => curr - 1);
+  }
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Card data={tutorialData[step]} onNextStep={handleNextStep} />
-    </>
+    <div className='flex justify-center items-center h-screen '>
+      <Card
+        currStep={step}
+        data={tutorialData[step]}
+        onNextStep={handleNextStep}
+        onPrevStep={handlePreviousStep}
+      />
+    </div>
   );
 }
 
--- src/data/tutorialData.ts
+++ src/data/tutorialData.ts
@@ -1,27 +1,27 @@
-import meditation from "../assets/images/meditation.svg";
-import programming from "../assets/images/programming.svg";
-import time_management from "../assets/images/time_managment.svg";
+import meditation from '../assets/images/meditation.svg';
+import programming from '../assets/images/programming.svg';
+import time_management from '../assets/images/time_managment.svg';
 
 export const tutorialData = [
   {
-    title: "Meditation",
+    title: 'Meditation',
     description:
       "Step into the serene world of meditation, where you'll find peace amidst life's chaos. Our carefully curated sessions cater to beginners and experienced practitioners alike, offering mindfulness techniques to help you center your mind and cultivate inner harmony.",
-    bgColor: "#3F9298",
+    bgColor: '#3F9298',
     image: meditation,
   },
   {
-    title: "Programming",
+    title: 'Programming',
     description:
-      "Next, embark on a coding adventure in our programming module. Whether you're a seasoned developer or a curious novice, our interactive lessons will empower you to master the art of coding. Dive into languages like Python, JavaScript, and more, and unlock the secrets of software development.n",
-    bgColor: "#C9CAD1",
+      "Next, embark on a coding adventure in our programming module. Whether you're a seasoned developer or a curious novice, our interactive lessons will empower you to master the art of coding. Dive into languages JavaScript, and more, and unlock the secrets of development.",
+    bgColor: '#C9CAD1',
     image: programming,
   },
   {
-    title: "Meditation",
+    title: 'Time Management',
     description:
       "Finally, take control of your time with our time management tools. From setting goals to organizing your schedule, we'll equip you with the skills you need to optimize your productivity and achieve your dreams. Say goodbye to procrastination and hello to a more efficient, balanced lifestyle.",
-    bgColor: "#FEC955",
+    bgColor: '#FEC955',
     image: time_management,
   },
 ];
