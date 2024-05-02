a0fe4eb0e6b3166a421aa68accfc5b5b88b77dc5
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-05-02 10:33:37 +0200

    Abstracted the logic from the Card to Indicator

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -1,12 +1,22 @@
 import { RxShadowOuter, RxTarget } from 'react-icons/rx';
+import { TutorialData } from '../types/tutorialData.type';
 
 type IndicatorProps = {
+  data: TutorialData[];
   currStep: number;
-  index: number;
 };
 
-export const Indicator = ({ currStep, index }: IndicatorProps) => {
-  const isActive = currStep === index;
-
-  return <>{isActive ? <RxTarget /> : <RxShadowOuter />}</>;
+export const Indicator = ({ data, currStep }: IndicatorProps) => {
+  return (
+    <div className='flex absolute bottom-12'>
+      {data.map((el, idx) => {
+        const isActive = idx === currStep;
+        return (
+          <span key={el.title}>
+            {isActive ? <RxTarget /> : <RxShadowOuter />}
+          </span>
+        );
+      })}
+    </div>
+  );
 };
